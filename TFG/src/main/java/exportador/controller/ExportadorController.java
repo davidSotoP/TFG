71c8760d535e99a2106c91ee740a4670cfc67ecf
David <davsotpon@alum.us.es>
2024-06-16 02:25:34 +0200

    Se añade @CrossOrigin

--- TFG/src/main/java/exportador/controller/ExportadorController.java
+++ TFG/src/main/java/exportador/controller/ExportadorController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -75,6 +76,7 @@ public class ExportadorController {
 		
 	}
 	
+	@CrossOrigin
 	@GetMapping(path = "/obtenerHistorial")
 	@ApiResponses(value = { 
 			@ApiResponse(responseCode = "200", description = "Historial obtenido con éxito", content = {
